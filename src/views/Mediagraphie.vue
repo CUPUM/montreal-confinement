73d2c2dd9a8eeec54468cc08cf363aeac49d0450
Emmanuel Beaudry Marchand <emmbema@gmail.com>
2020-07-10 09:56:46 -0400

    fiddle bug chapterNav?

--- src/views/Mediagraphie.vue
+++ src/views/Mediagraphie.vue
@@ -1,19 +1,22 @@
 <template>
-	<div id="bibliographie" class="view-scroll">
+	<div id="mediagraphie" class="view-scroll">
 		<h1>Médiagraphie</h1>
 		<div class="center-col">
 			<p>Contenu à venir</p>
-		</div>		
+		</div>
+		<div class="center-col">
+			<ChapterNav :previous="true" :next="false" />
+		</div>
 	</div>
 </template>
 
 <script>
-// @ is an alias to /src
+import ChapterNav from '@/components/ChapterNav'
 
 export default {
 	name: 'Mediagraphie',
 	components: {
-
+		ChapterNav
 	}
 }
 </script>
